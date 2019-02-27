6b17efcd05b25599c925720c19267c2da8634289
Jack <doherty9@purdue.edu>
2019-02-27 17:06:16 -0500

    fixing issue

--- lidar.cpp
+++ lidar.cpp
@@ -3,13 +3,8 @@
 #include "rplidar-sdk/include/rplidar.h"
 
 void lidar_connect() {
-<<<<<<< HEAD
-  rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver();
-  u_result res = lidar->connect("PATH", 256000);
-=======
   rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver(0);
   u_result res = lidar->connect("PATH", 256000);
->>>>>>> c993d178b66a40d2fa5b9aca37b01ef2af2983b5
 
   if(IS_OK(res)) {
     printf("YEEHAW\n");
