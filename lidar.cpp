3f544711a7a6063b62418adf7cded70978085cab
Jack <doherty9@purdue.edu>
2019-02-27 17:18:06 -0500

    still having compile issues

--- lidar.cpp
+++ lidar.cpp
@@ -3,7 +3,7 @@
 #include "rplidar-sdk/include/rplidar.h"
 
 void lidar_connect() {
-  rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver(0);
+  rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver();
   u_result res = lidar->connect("/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0", 256000);
 
   if(IS_OK(res)) {
