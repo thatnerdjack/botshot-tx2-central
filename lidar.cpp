b5dafa7ca431fd0794161411e82a58eeeca5ddf6
Jack <doherty9@purdue.edu>
2019-02-27 17:00:29 -0500

    lidar connect test

--- lidar.cpp
+++ lidar.cpp
@@ -3,5 +3,13 @@
 #include "rplidar-sdk/include/rplidar.h"
 
 void lidar_connect() {
-  RPlidarDriver *RPlidarDriver::CreateDriver(_u32 256000);
+  rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver();
+  u_result res = lidar->connect("PATH", 256000);
+
+  if(IS_OK(res)) {
+    printf("YEEHAW\n");
+    lidar->disconnect();
+  } else {
+    fprintf(stderr, "Failed to connect to LIDAR %08x\r\n", res);
+  }
 }
