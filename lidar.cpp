8e4f20a8aac48df83a4c4f39e431d89784cba996
Jack Doherty <doherty9@purdue.edu>
2019-02-27 17:09:33 -0500

    added lidar port

--- lidar.cpp
+++ lidar.cpp
@@ -4,7 +4,7 @@
 
 void lidar_connect() {
   rp::standalone::rplidar::RPlidarDriver *lidar = rp::standalone::rplidar::RPlidarDriver::CreateDriver(0);
-  u_result res = lidar->connect("PATH", 256000);
+  u_result res = lidar->connect("/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0", 256000);
 
   if(IS_OK(res)) {
     printf("YEEHAW\n");
